83df4d8661179923c8a17fd5d6a1be07d5a7985e
TanishkBhatt <tanishkbhatt25@gmail.com>
2026-03-03 13:55:15 -0800

    Updatation

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import streamlit as st
 import pandas as pd
 
 # ----------------------------------------------------
-data = pd.read_csv("dataset/match_records.csv")
+match_records = pd.read_csv("dataset/match_records.csv")
+batting_records = pd.read_csv("dataset/batting_records.csv")
+bowling_records = pd.read_csv("dataset/bowling_records.csv")
+
 st.set_page_config(
     page_title='Analytics Dashboard'
 )
@@ -10,11 +13,21 @@ st.title("ICC Men's T20 World Cup 2022 Matches | DashBoard")
 st.divider()
 # ----------------------------------------------------
 
+if "most_run_scorer" and "most_wicket_taker" not in st.session_state:
+    st.session_state.most_run_scorer = None
+    st.session_state.most_wicket_taker = None
+
 col1, col2, col3 = st.columns(3)
 with col1:
-    st.metric("WINNER", "ENGLAND", delta="+ SECOND TITLE")
+    st.metric("HOST", "AUSTRALIA", delta="+ FIRST TIME")
 with col2:
-    st.metric("RUNNER UP", "PAKISTAN", delta="- LOOSE BY 5 WICKETS")
+    st.metric("WINNER", "ENGLAND", delta="+ SECOND TITLE")
+with col3:
+    st.metric("RUNNER UP", "PAKISTAN", delta="- SECOND TIME")
+with col1:
+    st.metric("MOST RUNS", most_run_scorer, delta="+ RUNS")
+with col2:
+    st.metric("MOST WICKETS", most_wicket_taker, delta="+ WICKETS")
 with col3:
     st.metric("PLAYER OF THE TOURNAMENT", "SAM CURRAN", delta="+ 13 WICKETS")
 st.divider()
@@ -22,13 +35,18 @@ st.divider()
 # ----------------------------------------------------
 
 st.subheader("DataFrame Presentation")
-st.dataframe(data)
+with st.expander("VIEW MATCH RECORDS".title()):
+    st.dataframe(match_records)
+with st.expander("VIEW BATTING RECORDS".title()):
+    st.dataframe(batting_records)
+with st.expander("VIEW BOWLING RECORDS".title()):
+    st.dataframe(bowling_records)
 st.divider()
 # ----------------------------------------------------
 
 st.subheader("Most Number Of Wins By A Team")
 wins = {}
-for team in data["winner"]:
+for team in match_records["winner"]:
     wins[team] = wins.get(team, 0) + 1
 del wins["abandoned"]
 del wins["no result"]
@@ -44,10 +62,10 @@ st.divider()
 
 st.subheader("Most Player Of The Match Awards")
 potms = {}
-for player in data["potm"]:
+for player in match_records["potm"]:
     potms[player] = potms.get(player, 0) + 1
 
-potms = dict(sorted(potms.items(), key=lambda x:x[1], reverse=True)[:8])
+potms = dict(sorted(potms.items(), key=lambda x:x[1], reverse=True)[:10])
 
 potms_df = pd.DataFrame({'PLAYER': list(potms.keys()),
                          'NUMBER OF AWARDS': list(potms.values())})
@@ -57,4 +75,41 @@ st.bar_chart(potms_df.set_index("PLAYER"))
 st.divider()
 # ----------------------------------------------------
 
+st.subheader("Highest Run Scorers")
+highest_runs = {}
+
+for player, run in zip(batting_records["batsmanName"], batting_records["runs"]):
+    highest_runs[player.rstrip("(c)")] = highest_runs.get(player.rstrip("(c)"), 0) + run
+
+highest_runs = dict(sorted(highest_runs.items(), key=lambda x:x[1], reverse=True)[:15])
+
+runs_df = pd.DataFrame({"PLAYERS": list(highest_runs.keys()),
+                    "RUNS": list(highest_runs.values())})
+
+st.session_state.most_run_scorer = runs_df.loc[runs_df["RUNS"].idxmax(), "PLAYERS"]
+st.rerun()
+
+st.bar_chart(runs_df.set_index("PLAYERS"))
+st.divider()
+# ----------------------------------------------------
+
+st.subheader("Highest Wicket Taker")
+highest_wickets = {}
+
+for player, wicket in zip(bowling_records["bowlerName"], bowling_records["wickets"]):
+    highest_wickets[player.rstrip("(c)")] = highest_wickets.get(player.rstrip("(c)"), 0) + wicket
+
+highest_wickets = dict(sorted(highest_wickets.items(), key=lambda x:x[1], reverse=True)[:15])
+
+wickets_df = pd.DataFrame({"PLAYERS": list(highest_wickets.keys()),
+                    "WICKETS": list(highest_wickets.values())})
+
+st.session_state.most_wicket_taker = wickets_df.loc[wickets_df["WICKETS"].idxmax(), "PLAYERS"]
+st.rerun()
+
+st.bar_chart(wickets_df.set_index("PLAYERS"))
+st.divider()
+# -----------------------------------------------------
+
 st.caption("MADE BY TANISHK - A STUDENT AND A PROGRAMMER")
+# -----------------------------------------------------
